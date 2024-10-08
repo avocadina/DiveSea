5c42c8f04a0a9e6edf54ba56c013022b6830effc
leSchb <kurultae@yandex.ru>
2024-10-08 23:06:02 +0300

    Sectoin weekly-top NFT

--- js/main.js
+++ js/main.js
@@ -16,6 +16,41 @@ if (heroSlider) {
         hero1.classList.toggle('hero-slider__item--disabled');
         hero2.classList.toggle('hero-slider__item--disabled');
     });
+}
 
+//weekly slider
 
+const weeklySlider = document.querySelector('.weekly-slider__list');
+
+if (heroSlider) {   
+    const weeklyPrev = document.querySelector('.weekly-prev');
+    const weeklyNext = document.querySelector('.weekly-next');
+
+    weeklyNext.addEventListener('click', () => {
+        var width = 300;
+        var gap = 40;
+        var current = getComputedStyle(weeklySlider).marginLeft;
+
+        var minMargin = -(parseInt(width) + parseInt(gap))*2;
+
+        weeklySlider.style.marginLeft = parseInt(current) - parseInt(gap) - parseInt(width) + 'px';
+        var marginNumber = parseInt(current) - parseInt(gap) - parseInt(width);
+        if (marginNumber < minMargin) {
+            weeklySlider.style.marginLeft = '0px';
+        }
+    });
+
+    weeklyPrev.addEventListener('click', () => {
+        var width = 300;
+        var gap = 40;
+        var current = getComputedStyle(weeklySlider).marginLeft;
+
+        var maxMargin = (parseInt(width) + parseInt(gap))*2;
+
+        weeklySlider.style.marginLeft = parseInt(current) + parseInt(gap) + parseInt(width) + 'px';
+        var marginNumber = parseInt(current) + parseInt(gap) + parseInt(width);
+        if (marginNumber > maxMargin) {
+            weeklySlider.style.marginLeft = '0px';
+        }
+    });
 }
